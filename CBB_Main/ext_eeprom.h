65cc3dbaf736e8930880a8f3b3ae15444900853e
pksh-23514 <prabhat23514k@gmail.com>
2022-12-18 19:43:53 +0530

    External EEPROM file added

---
+++ CBB_Main/ext_eeprom.h
@@ -0,0 +1,12 @@
+#ifndef EXT_EEPROM_H
+#define EXT_EEPROM_H
+
+/* MACRO for the Slave Address */
+#define SLAVE_READ_EXT		0xA1
+#define SLAVE_WRITE_EXT		0xA0
+
+/* Function Declarations */
+void write_ext_eeprom (unsigned char address,  unsigned char data);
+unsigned char read_ext_eeprom (unsigned char address);
+
+#endif
